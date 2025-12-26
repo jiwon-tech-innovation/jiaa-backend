a55050bce617d90d3d5c6fb062361e08b31fcdc5
yoonio.h <wonji052271@gmail.com>
2025-12-26 16:53:22 +0900

    error fix

---
+++ auth-service/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+package io.github.jiwon;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SampleTest {
+    @Test
+    void healthCheck() {
+        assertTrue(true); // 무조건 성공하는 테스트! ✨
+    }
+}
