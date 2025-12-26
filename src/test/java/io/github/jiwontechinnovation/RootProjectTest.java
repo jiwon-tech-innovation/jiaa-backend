6af281bb476704bc2469c0fcd9ae5f08ef74fce2
yoonio.h <wonji052271@gmail.com>
2025-12-26 14:23:53 +0900

    간단한 Test코드 추가 및 Junit Dependency 추가

---
+++ src/test/java/io/github/jiwontechinnovation/RootProjectTest.java
@@ -0,0 +1,13 @@
+package io.github.jiwontechinnovation;
+
+import org.junit.jupiter.api.Test;
+
+class RootProjectTest {
+
+    @Test
+    void globalSanityCheck() {
+        // This test runs at the root project level
+        // Ideally used for checking global configurations or scripts
+        System.out.println("Root project test verification");
+    }
+}
